22572fd9b77cceed37f770c8678e1549a1ac342b
734800073@qq.com <734800073@qq.com>
2020-12-17 22:01:18 +0800

    21

--- spring-cloud-config-server-9091/src/main/java/com/zbh/example/springcloudconfigserver9091/SpringCloudConfigServer9091Application.java
+++ spring-cloud-config-server-9091/src/main/java/com/zbh/example/springcloudconfigserver9091/SpringCloudConfigServer9091Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCloudConfigServer9091Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudConfigServer9091Application.class, args);
     }
 
